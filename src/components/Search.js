208beeea9ce31cc774ef06d86459e425c806c00e
Jfontaine <jfontaine19@gmail.com>
2022-05-28 13:33:47 -0400

    Added the search buttons with classnames

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,8 +1,27 @@
 import React from "react";
 import "./Search.css";
+import Button from "@mui/material/Button";
+import MicOutlinedIcon from "@mui/icons-material/MicOutlined";
+import SatelliteAltOutlinedIcon from "@mui/icons-material/SatelliteAltOutlined";
 
 function Search() {
-  return <div className="search"></div>;
+  return (
+    <div className="search">
+      <div className="search__input">
+        <SatelliteAltOutlinedIcon className="search__inputIcon" />
+        <input />
+        <MicOutlinedIcon />
+      </div>
+      <div className="search__buttons">
+        <Button className="button" variant="outlined">
+          Search
+        </Button>
+        <Button className="button" variant="outlined">
+          I'm Feeling Lucky
+        </Button>
+      </div>
+    </div>
+  );
 }
 
 export default Search;
