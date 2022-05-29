141879202ce3c252b84c3cabcdc0dc06362e0617
Jfontaine <jfontaine19@gmail.com>
2022-05-29 13:01:29 -0400

    Added useEffect to the google search function

--- src/useGoogleSearch.js
+++ src/useGoogleSearch.js
@@ -1,14 +1,26 @@
-import { useState, useEffect } from 'react';
-import API_KEY from './keys';
+import { useState, useEffect } from "react";
+import API_KEY from "./keys";
 
-const CONTEXT_KEY = "4987d4b55ab42785b" // From: https://cse.google.com/cse/setup/basic?cx=4987d4b55ab42785b
+const CONTEXT_KEY = "4987d4b55ab42785b"; // From: https://cse.google.com/cse/setup/basic?cx=4987d4b55ab42785b
 
 const useGoogleSearch = (term) => {
-    const [data, setData] = useState(null);
+  const [data, setData] = useState(null);
 
-    useEffect(() => {
+  useEffect(() => {
+    const fetchData = async () => {
+      fetch(
+        `https://www.googleapis.com/customsearch/v1?key=${API_KEY}&cx=${CONTEXT_KEY}&q=${term}`
+      )
+        .then((response) => response.json())
+        .then((result) => {
+          setData(result);
+        });
+    };
 
-    }, [term])
-}
+    fetchData();
+  }, [term]);
 
-export default useGoogleSearch
+  return { data };
+};
+
+export default useGoogleSearch;
