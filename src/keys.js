e73595eaa55af22cb6613c7732804454abe83534
Jfontaine <jfontaine19@gmail.com>
2022-05-29 11:08:23 -0400

    File that contains  the API key from google

---
+++ src/keys.js
@@ -0,0 +1,3 @@
+export const API_KEY = AIzaSyAUDyiHcwFOqrW4w3Nx0XyxBg3zpK7U4sU; // From: https://developers.google.com/custom-search/v1/introduction#identify_your_application_to_google_with_api_key
+
+export default API_KEY;
